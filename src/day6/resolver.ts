cbf0a6268d0e446e1405b56e08ad508881dc5c1e
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-12-06 11:44:11 +0100

    feat(day6): part 2

--- src/day6/resolver.ts
+++ src/day6/resolver.ts
@@ -4,20 +4,56 @@ export const resolverPart1 = (input: string): number => {
     .reduce(
       (ac: string[][], group: string) => {
         if (group === "") {
-          ac.push([] as string[]);
+          ac.push([]);
           return ac;
         }
 
         const actualGroup = ac.length - 1;
         ac[actualGroup].push(...group.split(""));
-        ac[actualGroup] = ac[actualGroup].flat();
 
         return ac;
       },
-      [[]] as string[][]
+      [[]]
     )
     .map((group) => {
       return Array.from(new Set(group));
     })
     .reduce((ac, group) => ac + group.length, 0);
+};
+
+export const resolverPart2 = (input: string): number => {
+  return input
+    .split("\n")
+    .reduce(
+      (ac: string[][][], group: string) => {
+        if (group === "") {
+          ac.push([]);
+          return ac;
+        }
+
+        const actualGroup = ac.length - 1;
+        ac[actualGroup].push(group.split("") as string[]);
+
+        return ac;
+      },
+      [[]] as string[][][]
+    )
+    .map((group) => {
+      const personsInTheGroup = group.length;
+      const answers = group.flat().reduce((acc, answers: string) => {
+        if (!acc[answers]) {
+          acc[answers] = 1;
+          return acc;
+        }
+
+        acc[answers] = acc[answers] + 1;
+
+        return acc;
+      }, {} as Record<string, number>);
+
+      return Object.keys(answers).filter(
+        (key) => answers[key] === personsInTheGroup
+      ).length;
+    })
+    .reduce((ac, group) => ac + group, 0);
 };
--- tests/day6/resolver.spec.ts
+++ tests/day6/resolver.spec.ts
@@ -1,9 +1,9 @@
-import { resolverPart1 } from "../../src/day6/resolver";
+import { resolverPart1, resolverPart2 } from "../../src/day6/resolver";
 import { readFile } from "../../src/utils/utils";
 
 describe("day 6", () => {
   describe("part 1", () => {
-    describe("resolver 1", () => {
+    describe("resolver", () => {
       it("returns 11 with the test input", async () => {
         const input = readFile("day6/test.txt");
 
@@ -14,6 +14,22 @@ describe("day 6", () => {
         const input = readFile("day6/input.txt");
 
         expect(resolverPart1(input)).toEqual(6585);
+      });
+    });
+  });
+
+  describe("part 2", () => {
+    describe("resolver", () => {
+      it("returns 6 with the test input", async () => {
+        const input = readFile("day6/test.txt");
+
+        expect(resolverPart2(input)).toEqual(6);
+      });
+
+      it("returns the answer with the problem input", async () => {
+        const input = readFile("day6/input.txt");
+
+        expect(resolverPart2(input)).toEqual(3276);
       });
     });
   });
